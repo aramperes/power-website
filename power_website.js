15aa04e9ed1107669db69da74926fdf5ed5348e3
momothereal <momothereal.mc@gmail.com>
2018-01-12 21:29:29 -0500

    initial

---
+++ power_website.js
@@ -0,0 +1,72 @@
+console.log("Starting.");
+
+const config = require('./config.json');
+
+const PORT = config.port;
+const CLIENT_ID = config.auth.client_id;
+const CLIENT_SECRET = config.auth.client_secret;
+const REDIRECT_URI = config.auth.redirect_uri;
+
+const http = require('http');
+const url = require('url');
+const request = require('request');
+
+const tokens = [];
+
+/**
+ * @param {http.ClientRequest} req
+ * @param {http.ServerResponse} res
+ */
+function handleIncoming(req, res) {
+    let path = url.parse(req.url, true);
+    if (path.pathname === '/') {
+        res.writeHead(200);
+        res.write('welcome to power');
+        res.end();
+        return;
+    }
+    if (path.pathname === '/a/oauth') {
+        handleOauth(req, res, path);
+        return;
+    }
+    if (path.pathname === '/a/logout') {
+        handleLogout(req, res, path) ;
+        return;
+    }
+    res.writeHead(404);
+    res.write('Not found');
+    res.end();
+}
+
+function handleOauth(req, res, path) {
+    // get the code
+    if (!path.query['code']) {
+        res.writeHead('400');
+        res.write('No code!');
+        res.end();
+        return;
+    }
+    let oauth_code = path.query['code'];
+    let to_microsoft_query = {
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+        'code': oauth_code,
+        'redirect_uri': REDIRECT_URI,
+        'grant_type': 'authorization_code'
+    };
+    request.post('https://login.microsoftonline.com/common/oauth2/v2.0/token',
+        {form: to_microsoft_query},
+        (err, _res, body) => {
+            res.writeHead(200);
+            res.write(body);
+            res.end();
+        });
+}
+
+function handleLougout(req, res, path) {
+
+}
+
+http.createServer(handleIncoming).listen(PORT, function() {
+    console.log('server open');
+});
